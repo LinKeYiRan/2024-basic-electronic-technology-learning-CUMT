32a2ea9b177652ebba4dcc6bcbeacc0e35e3f79d
KahnZoe Nowenncht <136803397+LinKeYiRan@users.noreply.github.com>
2024-09-11 16:32:01 +0800

    Add files via upload

---
+++ 2-1 STM32工程模板/Objects/system_stm32f10x.d
@@ -0,0 +1,5 @@
+.\objects\system_stm32f10x.o: Start\system_stm32f10x.c
+.\objects\system_stm32f10x.o: Start\stm32f10x.h
+.\objects\system_stm32f10x.o: Start\core_cm3.h
+.\objects\system_stm32f10x.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_stm32f10x.o: Start\system_stm32f10x.h
---
+++ 2-1 STM32工程模板/User/main.c
@@ -0,0 +1,10 @@
+#include "stm32f10x.h"    // Device header
+//工程练习
+
+int main(void)
+{
+	while(1){
+		//检测工程文件是否正常
+	}
+	
+}
